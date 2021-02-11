3b2f4f73bb2bd95f4eae36b9a9f2778c9eaf402b
ROHITH-Singh <rs9113848@gmail.com>
2021-02-11 18:50:17 +0530

    Updates

---
+++ precache-manifest.c3a85725c56f309b485dc9209a98b365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5d92291e1f7b97714ae3bea0f8e101",
+    "url": "/whatsappclonev1/index.html"
+  },
+  {
+    "revision": "b46189a2c3927c617fb4",
+    "url": "/whatsappclonev1/static/css/main.f175692a.chunk.css"
+  },
+  {
+    "revision": "1f9f8abb3d41ee478241",
+    "url": "/whatsappclonev1/static/js/2.4f3ae51b.chunk.js"
+  },
+  {
+    "revision": "95e5393dbc94b774bee63119a26602ef",
+    "url": "/whatsappclonev1/static/js/2.4f3ae51b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b46189a2c3927c617fb4",
+    "url": "/whatsappclonev1/static/js/main.47cd910a.chunk.js"
+  },
+  {
+    "revision": "31a887979db9140ae803",
+    "url": "/whatsappclonev1/static/js/runtime-main.2bb77da6.js"
+  }
+]);
